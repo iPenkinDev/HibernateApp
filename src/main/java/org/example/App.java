8ad2c833a82bd28851f5c2e76ecc2608e501d420
Igor Penkin <igor.penkin.dev@gmail.com>
2023-02-10 13:45:29 +0300

    save entity in DB

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,11 +17,16 @@ public class App {
         Session session = sessionFactory.getCurrentSession();
 
         try {
-
             session.beginTransaction();
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+
+            Person person1 = new Person("Test1", 20);
+            Person person2 = new Person("Test2", 23);
+            Person person3 = new Person("Test3", 45);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+
             session.getTransaction().commit();
         } finally {
             sessionFactory.close();

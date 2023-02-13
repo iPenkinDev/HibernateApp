33f80c1887202dc999b5befe38273697e4da40a4
Igor Penkin <igor.penkin.dev@gmail.com>
2023-02-13 17:27:34 +0300

    ManyToMany relation test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,14 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,8 +16,8 @@ import org.hibernate.cfg.Configuration;
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
 
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
@@ -52,8 +56,21 @@ public class App {
 //            person.setPassport(passport);
 //            session.save(person);
             //Read from DB (OneToOne)
-            Passport passport = session.get(Passport.class, 1);
-            System.out.println(passport.getPerson().getName());
+//            Passport passport = session.get(Passport.class, 1);
+//            System.out.println(passport.getPerson().getName());
+
+            Movie movie = new Movie("Pulp fiction", 1994);
+            Actor actor1 = new Actor("Harvey Keytel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+
+            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+
+            session.save(movie);
+            session.save(actor1);
+            session.save(actor2);
+
             session.getTransaction().commit();
         } finally {
             sessionFactory.close();
--- src/main/java/org/example/model/Actor.java
+++ src/main/java/org/example/model/Actor.java
@@ -1,15 +1,14 @@
 package org.example.model;
 
-import org.hibernate.annotations.Cascade;
-
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name = "Person")
-public class Person {
+@Table(name = "Actor")
+public class Actor {
 
     @Id
-    @Column(name = "id")
+    @Column(name = "actor_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
@@ -19,15 +18,19 @@ public class Person {
     @Column(name = "age")
     private int age;
 
-    @OneToOne(mappedBy = "person")
-    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
-    private Passport passport;
+    @ManyToMany
+    @JoinTable(
+            name = "Actor_Movie",
+            joinColumns = @JoinColumn(name = "actor_id"),
+            inverseJoinColumns = @JoinColumn(name = "movie_id")
+    )
+    private List<Movie> movies;
 
-    public Person() {
+    public Actor(){
 
     }
 
-    public Person(String name, int age) {
+    public Actor(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -56,18 +59,17 @@ public class Person {
         this.age = age;
     }
 
-    public Passport getPassport() {
-        return passport;
+    public List<Movie> getMovies() {
+        return movies;
     }
 
-    public void setPassport(Passport passport) {
-        this.passport = passport;
-        passport.setPerson(this);
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
     }
 
     @Override
     public String toString() {
-        return "Person{" +
+        return "Actor{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
